f61e0e7a51ee3dfdd251ffcfda15a26df81711dc
Tim Lantz <tlantz@gmail.com>
2020-07-09 15:50:46 -0500

    add react-hot-loader

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { hot } from "react-hot-loader";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className="App">
         <h1> React boilerplate </h1>
       </div>
     );
   }
 }
 
-export default App;
+export default hot(module)(App);
